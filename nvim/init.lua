e345fbd4135f8f41b8505909f2944d686218d33d
applejoe <mail@the-chameleon.de>
2025-03-08 01:38:06 +0100

    add nvim

---
+++ nvim/init.lua
@@ -0,0 +1,79 @@
+-- https://github.com/hendrikmi/dotfiles/blob/main/nvim
+require("core.options")
+require("core.keymaps")
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" },
+		require("plugins.colorscheme"),
+		-- require 'plugins.neotree',
+		-- require 'plugins.bufferline',
+		-- require 'plugins.lualine',
+		-- require 'plugins.treesitter',
+		-- require 'plugins.telescope',
+		-- require 'plugins.lsp',
+		-- require 'plugins.autocompletion',
+		-- require 'plugins.none-ls',
+		-- require 'plugins.alpha',
+		-- require 'plugins.indent-blankline',
+		-- require 'plugins.lazygit',
+		-- require 'plugins.comment',
+		-- require 'plugins.debug',
+		-- require 'plugins.gitsigns',
+		-- require 'plugins.database',
+		-- require 'plugins.misc',
+		-- require 'plugins.harpoon',
+		-- require 'plugins.avante',
+		-- require 'plugins.chatgpt',
+		-- require 'plugins.aerial',
+		-- require 'plugins.vim-tmux-navigator',
+	},
+	checker = { enabled = true },
+	install = { colorscheme = { "nord" } },
+}, {
+	ui = {
+		-- If you have a Nerd Font, set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
+})
+
+-- Function to check if a file exists
+-- local function file_exists(file)
+--   local f = io.open(file, 'r')
+--   if f then
+--     f:close()
+--     return true
+--   else
+--     return false
+--   end
+-- end
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
