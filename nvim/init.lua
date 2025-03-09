013b03f5008f57307c5d6e5a022d5d4a224cc62c
applejoe <mail@the-chameleon.de>
2025-03-09 16:47:48 +0100

    chore: remove single plugin require

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,62 +7,42 @@ require("core.autocommands")
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    error("Error cloning lazy.nvim:\n" .. out)
+  end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
-		-- require("plugins.colorscheme"),
-		-- require 'plugins.neotree',
-		-- require 'plugins.bufferline',
-		-- require 'plugins.lualine',
-		-- require 'plugins.treesitter',
-		-- require 'plugins.telescope',
-		-- require 'plugins.lsp',
-		-- require 'plugins.autocompletion',
-		-- require 'plugins.none-ls',
-		-- require 'plugins.alpha',
-		-- require 'plugins.indent-blankline',
-		-- require 'plugins.lazygit',
-		-- require 'plugins.comment',
-		-- require 'plugins.debug',
-		-- require 'plugins.gitsigns',
-		-- require 'plugins.database',
-		-- require 'plugins.misc',
-		-- require 'plugins.harpoon',
-		-- require 'plugins.avante',
-		-- require 'plugins.chatgpt',
-		-- require 'plugins.aerial',
-		-- require 'plugins.vim-tmux-navigator',
-	},
-	checker = { enabled = true },
-	install = { colorscheme = { "nord" } },
+  spec = {
+    { import = "plugins" },
+    -- require("plugins.colorscheme"),
+    -- require 'plugins.neotree',
+  },
+  checker = { enabled = true },
+  install = { colorscheme = { "nord" } },
 }, {
-	ui = {
-		-- If you have a Nerd Font, set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	},
+  ui = {
+    -- If you have a Nerd Font, set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
+    },
+  },
 })
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
